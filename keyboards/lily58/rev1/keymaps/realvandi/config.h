9cc342c7798030fa66f8787167415bce48a4c16b
Dylan Alvandi <g.dylanalvandi@gmail.com>
2024-08-11 23:02:03 +0200

    feat: Delete test keymap, create realvandi keymap based on vial keymap

--- keyboards/lily58/rev1/keymaps/realvandi/config.h
+++ keyboards/lily58/rev1/keymaps/realvandi/config.h
@@ -20,36 +20,30 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
+/* VIAL UID for Lily58*/
+#define VIAL_KEYBOARD_UID {0x7E, 0xFD, 0xFC, 0x5B, 0x7D, 0x39, 0x48, 0x06}
+
+/* VIAL secure unlock keystroke - currently both big keys (typ. SPACE/ENTER) */
+#define VIAL_UNLOCK_COMBO_ROWS {4, 9}
+#define VIAL_UNLOCK_COMBO_COLS {4, 4}
+
+/* Space reduction */
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
+#define VIAL_TAP_DANCE_ENTRIES 4
+#undef LOCKING_SUPPORT_ENABLE
+#undef LOCKING_RESYNC_ENABLE
+#define NO_ACTION_ONESHOT
+
 /* Select hand configuration */
 
 #define MASTER_LEFT
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
-#define QUICK_TAP_TERM 0
+#define USE_SERIAL_PD2
+
+#define TAPPING_FORCE_HOLD
 #define TAPPING_TERM 100
-/*#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET*/
 
-#undef RGBLIGHT_LED_COUNT
-#define RGBLIGHT_EFFECT_BREATHING
-#define RGBLIGHT_EFFECT_RAINBOW_MOOD
-#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-#define RGBLIGHT_EFFECT_SNAKE
-#define RGBLIGHT_EFFECT_KNIGHT
-#define RGBLIGHT_EFFECT_CHRISTMAS
-#define RGBLIGHT_EFFECT_STATIC_GRADIENT
-#define RGBLIGHT_EFFECT_RGB_TEST
-#define RGBLIGHT_EFFECT_ALTERNATING
-#define RGBLIGHT_EFFECT_TWINKLE
-#define RGBLIGHT_LED_COUNT 27
-#define RGBLIGHT_LIMIT_VAL 120
-#define RGBLIGHT_HUE_STEP 10
-#define RGBLIGHT_SAT_STEP 17
-#define RGBLIGHT_VAL_STEP 17
 
-// Underglow
-/*
-#undef RGBLIGHT_LED_COUNT
-#define RGBLIGHT_LED_COUNT 14    // Number of LEDs
-#define RGBLIGHT_SLEEP
-*/
--- keyboards/lily58/rev1/keymaps/realvandi/rules.mk
+++ keyboards/lily58/rev1/keymaps/realvandi/rules.mk
@@ -1,24 +1,19 @@
-LTO_ENABLE = yes            # Link Time Optimization enabled
-BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = yes		# Mouse keys
-EXTRAKEY_ENABLE = no        # Audio control and System control
-CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = no            # N-Key Rollover
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-AUDIO_ENABLE = no           # Audio output
-RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
-SWAP_HANDS_ENABLE = no      # Enable one-hand typing
-OLED_ENABLE = yes           # OLED display
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+OLED_ENABLE = yes
+MOUSEKEY_ENABLE = yes
+EXTRAKEY_ENABLE = yes
+WPM_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+COMBO_ENABLE = yes
+GRAVE_ESC_ENABLE = no
+MAGIC_ENABLE = no
+BOOTMAGIC_ENABLE = no
 
-# Bootloader selection
-BOOTLOADER = rp2040
+COMMAND_ENABLE = no
+CONSOLE_ENABLE = no
 
-# If you want to change the display of OLED, you need to change here
-SRC +=  ./lib/rgb_state_reader.c \
-        ./lib/layer_state_reader.c \
-        ./lib/logo_reader.c \
-        ./lib/keylogger.c \
-        # ./lib/mode_icon_reader.c \
-        # ./lib/host_led_state_reader.c \
-        # ./lib/timelogger.c \
+QMK_SETTINGS = yes
+LTO_ENABLE = yes
+
+KEY_OVERRIDE_ENABLE = no
